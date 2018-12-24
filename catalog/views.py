db9d054bfd034201bca51248fae72cec3ac7d036
Matthew <mxmdmiller@gmail.com>
2018-12-24 01:26:41 -0500

    Initial commit

---
+++ catalog/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre
+
+# Create your views here.
+
+def index(request):
+    """View function for the home page of the site"""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.all().count()
+
+    context = {
+        'num_books' : num_books,
+        'num_instances' : num_instances,
+        'num_instances_available' : num_instances_available,
+        'num_authors' : num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
