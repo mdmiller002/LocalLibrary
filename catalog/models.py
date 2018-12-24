7ebc3f41acbd4e22536d3d795a3311df88dd98c5
Matthew <mxmdmiller@gmail.com>
2018-12-24 17:28:53 -0500

    Added author and book pages

--- catalog/models.py
+++ catalog/models.py
@@ -22,6 +22,9 @@ class Book(models.Model):
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
+    class Meta:
+        ordering = ['title']
+
     def __str__(self):
         return self.title
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from catalog.models import Book, Author, BookInstance, Genre
 
 # Create your views here.
@@ -20,3 +21,17 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
